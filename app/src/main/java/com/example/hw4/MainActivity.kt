e887187abb7e35adcd7795e3c1da9b07098fb38d
Pikin <pikin@bk.ru>
2024-04-24 11:41:02 +0500

    first commit on work comp

---
+++ app/src/main/java/com/example/hw4/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.hw4
+
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val nameInputLayout: TextInputLayout = findViewById(R.id.name_input_layout)
+        nameInputLayout.endIconMode=TextInputLayout.END_ICON_CLEAR_TEXT
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+
+        }
+
+
+
+
+
+
+
+
+    }
