8da93b049cab06ee58295d4d243b2dce593e8444
pikin <pikin@bk.ru>
2024-04-24 22:29:21 +0500

    mainActivity changed

--- app/src/main/java/com/example/hw4/MainActivity.kt
+++ app/src/main/java/com/example/hw4/MainActivity.kt
@@ -1,49 +1,33 @@
 package com.example.hw4
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.CompoundButton
-import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hw4.databinding.ActivityMainBinding
-import com.google.android.material.checkbox.MaterialCheckBox
-import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("UseSwitchCompatOrMaterialCode")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val nameInputLayout: TextInputLayout = findViewById(R.id.name_input_layout)
-        nameInputLayout.endIconMode = TextInputLayout.END_ICON_CLEAR_TEXT
+        val progressNum = (0..100).random()
 
-        val progressNum = (0..100).random().toInt()
-        val progressBar = findViewById<LinearProgressIndicator>(R.id.progress_bar)
-        val switch = findViewById<Switch>(R.id.receive_notifications_switch)
-//        val checkNews = findViewById<MaterialCheckBox>(R.id.checkbox_about_news)
-//        val checkAuthorization = findViewById<MaterialCheckBox>(R.id.checkbox_about_authorization)
-
+        binding.nameInputLayout.endIconMode = TextInputLayout.END_ICON_CLEAR_TEXT
         binding.progressPoints.text = toStr(progressNum)
-        progressBar.progress = progressNum
+        binding.progressBar.progress = progressNum
 
-        switch.setOnCheckedChangeListener { _,
-                                            isChecked ->
+        binding.receiveNotificationsSwitch.setOnCheckedChangeListener { _, isChecked ->
             binding.checkboxAboutNews.isEnabled = isChecked
             binding.checkboxAboutAuthorization.isEnabled = isChecked
-
         }
     }
-
 
     private fun toStr(str: Int): String {
         return "$str/100"
     }
-
-
 }
