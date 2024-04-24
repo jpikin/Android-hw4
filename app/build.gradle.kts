bc9bcd8fb4641d14a7b6184a626b016d91a5c406
pikin <pikin@bk.ru>
2024-04-24 22:18:50 +0500

    switch working correct

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,17 +33,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-}
+    buildFeatures {
+        viewBinding = true
+    }
 
-dependencies {
+    dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        implementation(libs.androidx.activity)
+        implementation(libs.androidx.constraintlayout)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
 
+    }
 }
--- app/src/main/java/com/example/hw4/MainActivity.kt
+++ app/src/main/java/com/example/hw4/MainActivity.kt
@@ -1,37 +1,49 @@
 package com.example.hw4
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
-import android.widget.RadioGroup
+import android.widget.CompoundButton
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.hw4.databinding.ActivityMainBinding
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val nameInputLayout: TextInputLayout = findViewById(R.id.name_input_layout)
-        nameInputLayout.endIconMode=TextInputLayout.END_ICON_CLEAR_TEXT
+        nameInputLayout.endIconMode = TextInputLayout.END_ICON_CLEAR_TEXT
 
-        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+        val progressNum = (0..100).random().toInt()
+        val progressBar = findViewById<LinearProgressIndicator>(R.id.progress_bar)
+        val switch = findViewById<Switch>(R.id.receive_notifications_switch)
+//        val checkNews = findViewById<MaterialCheckBox>(R.id.checkbox_about_news)
+//        val checkAuthorization = findViewById<MaterialCheckBox>(R.id.checkbox_about_authorization)
+
+        binding.progressPoints.text = toStr(progressNum)
+        progressBar.progress = progressNum
+
+        switch.setOnCheckedChangeListener { _,
+                                            isChecked ->
+            binding.checkboxAboutNews.isEnabled = isChecked
+            binding.checkboxAboutAuthorization.isEnabled = isChecked
 
         }
-
-
-
-
-
-
-
-
     }
+
+
+    private fun toStr(str: Int): String {
+        return "$str/100"
+    }
+
+
+}
