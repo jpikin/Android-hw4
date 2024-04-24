4f90175952aaac0ccfa8feac2f3f73294cd99a15
pikin <pikin@bk.ru>
2024-04-25 00:27:14 +0500

    listeners started

--- app/src/main/java/com/example/hw4/MainActivity.kt
+++ app/src/main/java/com/example/hw4/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.hw4
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isNotEmpty
 import com.example.hw4.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputLayout
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +20,23 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        println("program started")
+//        var  = false
+        var phoneFlag = false
+        var radioFlag = false
+        var switchFlag = false
+
+        var nameFlag: Boolean by Delegates.observable(false) { _, old, new ->
+            if (new != old) {
+                binding.saveButton.isEnabled = true
+
+            } else {
+                binding.saveButton.isEnabled = false
+
+            }
+        }
+
+
         val progressNum = (0..100).random()
 
         binding.nameInputLayout.endIconMode = TextInputLayout.END_ICON_CLEAR_TEXT
@@ -24,7 +46,39 @@ class MainActivity : AppCompatActivity() {
         binding.receiveNotificationsSwitch.setOnCheckedChangeListener { _, isChecked ->
             binding.checkboxAboutNews.isEnabled = isChecked
             binding.checkboxAboutAuthorization.isEnabled = isChecked
+            if (!isChecked) {
+                switchFlag = true
+
+            } else if (binding.checkboxAboutNews.isActivated || binding.checkboxAboutAuthorization.isActivated) {
+                switchFlag = true
+
+            }
         }
+
+        binding.editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                val text = s.toString()
+                if (text.isNotEmpty()) {
+                    nameFlag = true
+                }
+                else {
+                    nameFlag = false
+                }
+            }
+        })
+        binding.editPhone.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                val text = s.toString()
+                if (text.isNotEmpty()) {
+                    phoneFlag = true
+
+                }
+            }
+        })
     }
 
     private fun toStr(str: Int): String {
