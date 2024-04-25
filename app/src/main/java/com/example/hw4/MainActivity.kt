29118fc1c21e1185c866734ad11b5f4a60551dd8
Pikin <pikin@bk.ru>
2024-04-25 15:10:45 +0500

    toast added

--- app/src/main/java/com/example/hw4/MainActivity.kt
+++ app/src/main/java/com/example/hw4/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hw4.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var button: Button
+
 
     private var nameFlag: Boolean by Delegates.observable(false) { _, _, _ ->
         updateButtonState()
@@ -25,44 +30,51 @@ class MainActivity : AppCompatActivity() {
     private var switchFlag: Boolean by Delegates.observable(false) { _, _, _ ->
         updateButtonState()
     }
-    private fun updateButtonState(){
-        button.isEnabled = nameFlag&&phoneFlag&&radioFlag&&switchFlag
+
+    private fun updateButtonState() {
+        button.isEnabled = nameFlag && phoneFlag && switchFlag && radioFlag
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        button = findViewById(R.id.saveButton)
+
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val progressNum = (0..100).random()
-
+        var newsFlag = false
+        var authorFlag = false
+        var switchCheck = true
+        button = findViewById(R.id.saveButton)
         binding.nameInputLayout.endIconMode = TextInputLayout.END_ICON_CLEAR_TEXT
         binding.progressPoints.text = toStr(progressNum)
         binding.progressBar.progress = progressNum
 
+        binding.checkboxAboutNews.setOnCheckedChangeListener { _, isChecked ->
+            newsFlag = isChecked
+            switchFlag = setUpSwitchFlag(newsFlag, authorFlag, switchFlag)
+        }
+        binding.checkboxAboutAuthorization.setOnCheckedChangeListener { _, isChecked ->
+            authorFlag = isChecked
+            switchFlag = setUpSwitchFlag(newsFlag, authorFlag, switchFlag)
+
+        }
         binding.receiveNotificationsSwitch.setOnCheckedChangeListener { _, isChecked ->
+            switchCheck = isChecked
             binding.checkboxAboutNews.isEnabled = isChecked
             binding.checkboxAboutAuthorization.isEnabled = isChecked
-            if (!isChecked) {
-                switchFlag = true
-
-            } else if (binding.checkboxAboutNews.isActivated || binding.checkboxAboutAuthorization.isActivated) {
-                switchFlag = true
-
-            }
+            switchFlag = setUpSwitchFlag(newsFlag, authorFlag, switchFlag)
         }
-
         binding.editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {
                 val text = s.toString()
-                nameFlag = text.isNotEmpty()&&text.length<41
+                nameFlag = text.isNotEmpty() && text.length < 41
             }
         })
-
         binding.editPhone.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
@@ -71,8 +83,27 @@ class MainActivity : AppCompatActivity() {
                 phoneFlag = text.isNotEmpty()
             }
         })
-
+        binding.radioGroup.setOnCheckedChangeListener {group, checkedId ->
+            val radioButton: RadioButton = findViewById(checkedId)
+            radioFlag = radioButton.isChecked
+        }
         updateButtonState()
+        binding.saveButton.setOnClickListener {
+            Toast.makeText(this, "Данные сохранены", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setUpSwitchFlag(newsFlag: Boolean,
+                          authorFlag: Boolean,
+                          switchCheck: Boolean
+                          ) : Boolean {
+
+        return if(!switchCheck)
+            true
+        else if (authorFlag || newsFlag)
+            true
+        else
+            false
     }
 
     private fun toStr(str: Int): String {
